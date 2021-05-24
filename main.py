99ee098b9bf3bbdccb6a98665375f3639b9a79d6
Jose S. Daniel <djose1164@gmail.com>
2021-05-24 16:02:23 -0400

    GUI created.

---
+++ main.py
@@ -0,0 +1,31 @@
+from kivy import require
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.anchorlayout import AnchorLayout
+
+from os import listdir
+
+kv_path: str = "./kv/"
+for kv in listdir(kv_path):
+    print(kv)
+    Builder.load_file(kv_path + kv)
+
+require("1.11.1")
+
+
+class ComicCreator(AnchorLayout):
+    pass
+
+
+class ComicCreatorApp(App):
+    def build(self):
+        return ComicCreator()
+
+
+def main():
+    app = ComicCreatorApp()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
