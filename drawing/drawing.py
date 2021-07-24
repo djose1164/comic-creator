5b646fa9482b12f333f3f796c6e711c73ccf8957
Jose S. Daniel <djose1164@gmail.com>
2021-07-23 21:21:14 -0400

    minor fixes

--- drawing/drawing.py
+++ drawing/drawing.py
@@ -4,10 +4,10 @@ from kivy.uix.relativelayout import RelativeLayout
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-Builder.load_file("drawing/drawing.kv")
-# Builder.load_file("drawing/drawing_line.kv")
-# Builder.load_file("drawing/drawing_img.kv")
-# Builder.load_file("./kv/drawing/drawing_order.kv")
+# Builder.load_file("drawing.kv")
+# Builder.load_file("drawing_line.kv")
+# Builder.load_file("drawing_img.kv")
+Builder.load_file("drawing_order.kv")
 require("1.11.1")
 
 
@@ -15,14 +15,14 @@ class DrawingSpace(RelativeLayout):
     pass
 
 
-class DrawingApp(App):
+class _DrawingApp(App):
     def build(self):
         return DrawingSpace()
 
 
 def main():
     Window.size = 300, 100
-    app = DrawingApp()
+    app = _DrawingApp()
     app.run()
 
 
